b31b1c69b7b23f45e89dbb402270d9fd76ff95ac
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-04-10 16:40:17 -0400

    removed unused files and codemaided

--- AppointmentScheduling/AppShell.xaml.cs
+++ AppointmentScheduling/AppShell.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using AppointmentScheduling.ViewModels;
-using AppointmentScheduling.Views;
-using Xamarin.Forms;
-
-namespace AppointmentScheduling
+﻿namespace AppointmentScheduling
 {
   public partial class AppShell : Xamarin.Forms.Shell
   {
     public AppShell()
     {
       InitializeComponent();
-      Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-      Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
-    }
-
-    private async void OnMenuItemClicked(object sender, EventArgs e)
-    {
-      await Shell.Current.GoToAsync("//LoginPage");
     }
   }
 }
